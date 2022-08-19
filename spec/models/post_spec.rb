0a4f6aaf0310fd8791e339917e86c6cc07f5e7e6
NatiG <nati.g5684@gmail.com>
2022-08-19 19:37:14 +0300

    Add tests for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+RSpec.describe Post, type: :model do
+  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  post = Post.create(user: user, title: 'Hello', text: 'This is my first post')
 
-  before {subject.save}
-
-  it 'name should be present' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  it 'title should be present' do
+    post.title = nil
+    expect(post).to_not be_valid
   end
 
-  it 'post_counter should be greater than or equal to 0' do
-    subject.posts_counter = 0
-    expect(subject).to be_valid
+  it 'title must not exceed 250 characters' do
+    post.title = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+     parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, 
+     fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer 
+     tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam 
+     lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+     Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, 
+     blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus
+     tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,'
+    expect(post).to_not be_valid
+  end
+  it 'comments_counter should be greater than or equal to 0' do
+    post.comments_counter = -1
+    expect(post).to_not be_valid
+  end
+
+  it 'likes_counter should be greater than or equal to 0' do
+    post.likes_counter = -1
+    expect(post).to_not be_valid
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
-
-  before {subject.save}
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
 
   it 'name should be present' do
     subject.name = nil
