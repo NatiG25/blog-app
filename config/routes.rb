14b63189789f1ef80bb3602df50570e31dee762a
NatiG <nati.g5684@gmail.com>
2022-08-23 10:55:22 +0300

    Delete unnecessary view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
-  # resources :posts
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
