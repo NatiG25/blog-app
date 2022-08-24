bbdb7bb82f73fbf776c540d5f6adadafe2c02c20
NatiG <nati.g5684@gmail.com>
2022-08-24 22:11:24 +0300

    Display details of a specific user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def index
     @users = User.all
   end
 
   def show; end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
