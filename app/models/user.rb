4af513640c615a2abd2782d66df8764339abda86
NatiG <nati.g5684@gmail.com>
2022-08-26 20:05:08 +0300

    Add styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  # validates :name, presence: true
-  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   has_many :posts, foreign_key: :user_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
+
+  # validates :name, presence: true
+  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end
