70ed07b759eacf79b5ce41896fb2d47e0d4ff5f4
NatiG <nati.g5684@gmail.com>
2022-09-03 20:17:42 +0300

    Create an api & v1 folders

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,6 +3,8 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
+
+
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, "\\1en"
 #   inflect.singular /^(ox)en/i, "\\1"
@@ -11,6 +13,6 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym "API"
+end
